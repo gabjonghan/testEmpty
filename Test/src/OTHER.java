97b6920078146975c81d4d1de27be37efcb88ab9
Gabjong Han <gabjonghan@gmail.com>
2015-11-12 18:09:21 +0900

    요거는 커밋만 할까요

---
+++ Test/src/OTHER.java
@@ -0,0 +1,14 @@
+/*
+ * @(#)OTHER.java $version 2015. 11. 12.
+ *
+ * Copyright 2015 NHN Corp. All rights Reserved. 
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+
+/**
+ * @author Han, Gabjong (KR16278) - 2015. 11. 12. 오후 6:07:07
+ */
+public class OTHER {
+
+}
