34af5f889e8ceaf4bb11df7bfda565c9aa184bea
gabjonghan <gabjonghan@gmail.com>
2015-11-12 18:20:05 +0900

    update commit on 깃허브

--- Test/src/OTHER.java
+++ Test/src/OTHER.java
@@ -1,6 +1,6 @@
 /*
  * @(#)OTHER.java $version 2015. 11. 12.
- *
+ * 내부적으로 먼가를 바꾸면 어떻게 되나요;
  * Copyright 2015 NHN Corp. All rights Reserved. 
  * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
